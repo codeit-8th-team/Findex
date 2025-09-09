71d3785693bfeecdfca57a92e11880de3d236ed9
yunji1014 <7787yunji@gmail.com>
2025-09-09 10:18:55 +0900

    초기 셋팅

--- src/main/java/com/findex/service/Service.java
+++ src/main/java/com/findex/service/Service.java
@@ -1,3 +1,18 @@
 package com.findex.service;
 
-public class Service {}
+import com.findex.config.OpenApiProperties;
+import lombok.RequiredArgsConstructor;
+
+@org.springframework.stereotype.Service
+@RequiredArgsConstructor
+public class Service {
+
+  private final OpenApiProperties props; // 생성자 주입
+
+  public String callSomething() {
+    String baseUrl = props.baseUrl();
+    String serviceKey = props.serviceKey(); // 여기서 사용
+    // ... 호출 코드
+    return "...";
+  }
+}
